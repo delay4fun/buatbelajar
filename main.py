4a04e22bca5179008b326eca34af1eed1e14f21e
delay4fun <84764441+delay4fun@users.noreply.github.com>
2021-06-03 16:18:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # test print
-print = ('Hello')
+print('Hello')
 
-print = ("Hello World")
+from time import sleep
+
+value1 = [0]
+value2 = 0
+prin = ("Hello World")
+
+for i in value1:
+  value2 += 1
+  value1.append(value2)
+  sleep(0.5)
+  print(prin)
+  
