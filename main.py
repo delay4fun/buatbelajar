da18ad8ac74df4d73b89dbcf98dbcb37bc48c65a
delay4fun <84764441+delay4fun@users.noreply.github.com>
2021-06-03 16:12:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-"""
- Homework Assignment #1: Variables
- {pirple.com}
- herfano.tm@gmail.com
-"""
-# These attribute artist, album, song, genre, duration, year
-print("\n===What's your favorite song?===\n")
+# test print
+print = ('Hello')
 
-artist = "Nirvana"
-album = "Nevermind"
-song = "Smells Like Teen Spirit"
-genre = "Grunge"
-durationInSec = 410.4
-yearReleased = 1991
-
-# variable are strings
-print("artist :", artist)
-print("album :", album)
-print("song :", song)
-print("genre :", genre)
-# varible is float
-print("duration :", durationInSec)
-# varible is integer
-print("year :", yearReleased)
-
+print = ("Hello World")
