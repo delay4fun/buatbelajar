ea4350abac370eb5435908d644c5ef4b991877c1
delay4fun <84764441+delay4fun@users.noreply.github.com>
2021-06-12 00:26:35 +0700

    Update testSquare.py

--- testSquare.py
+++ testSquare.py
@@ -1,10 +1,12 @@
 # # make a square
-# Clever Programmer (qazi)
+## Clever Programmer (qazi)
 
 import turtle
 from time import sleep
 
 testSquare = turtle.Turtle()
+
+## this use for loop
 val1 = [0]
 val2 = 0
 
@@ -18,8 +20,15 @@ def square():
     testSquare.left(100) # comment this line and uncomment below
     # testSquare.forward(100)
 # square()
+
+## use for loop
 for i in val1:
     val2 += 1
     val1.append(val2)
     print(square())
     sleep(0.1)
+
+## use while loop    
+# while True:
+#     square()
+#     sleep(0.1)
