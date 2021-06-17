543044e827e632ce66d8af69d454d59073e6cc99
delay4fun <84764441+delay4fun@users.noreply.github.com>
2021-06-17 17:34:54 +0700

    Create pirple10.py

---
+++ pirple10.py
@@ -0,0 +1,59 @@
+# ## Introduction to Importing
+
+# import random as r 
+
+# # random.seed(1)
+# randInt = r.randint(0 ,10)
+# print(randInt)
+
+# randFloat = r.random()
+# print(randFloat)
+
+# randUniform = r.uniform(1, 11)
+# print(randUniform)
+
+### ====================================
+## Alternative Import Methods
+
+# import random as r
+
+# simpleList = [1,3,5,7,11]
+# pickElement = r.choice(simpleList)
+# print(pickElement)
+# print(simpleList)
+# r.shuffle(simpleList)
+# print(simpleList)
+
+### ======================================
+## The Time Library
+
+# import time
+
+# currentTime = time.time()
+# print("Hello")
+# print("World")
+# pastTime = time.time()
+# print(pastTime - currentTime)
+
+# print("1")
+# time.sleep(3)
+# print("2")
+
+# for i in range(1, 11):
+#     print(i)
+#     time.sleep(1)
+
+
+### ==========================
+## The Math Library
+
+import math
+
+val = 3
+
+sqrtVal = math.sqrt(val)
+print(sqrtVal)
+
+expVal = math.exp(val)
+print(expVal)
+
